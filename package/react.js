9e66506df6ccfc86f0c7cdb53f0946cc5693d261
Kerwin Zheng <1747269691@qq.com>
2026-03-31 00:12:55 +0800

    docs(package): add inline comments and usage example to react store

--- package/react.js
+++ package/react.js
@@ -4,13 +4,27 @@ import { createStore } from "./vanilla.js";
 const useStore = (api, selector) => {
 	const slice = useSyncExternalStore(
 		api.subscribe,
+		// Client Snapshot
 		useCallback(() => selector(api.getState()), [api, selector]),
+		// Server Snapshot
 		useCallback(() => selector(api.getInitState()), [api, selector]),
 	);
 
 	return slice;
 };
 
+/**
+ * const userStore = create((set) => {
+ * 		name: "",
+ * 		setName: (name) => set({name: name}))
+ * })
+ *
+ * Comp A:
+ *  function A() {
+ * 		const { name } = userStore()
+ * 		const name = userStore(state => state.name)
+ * }
+ */
 const create = (createState) => {
 	const api = createStore(createState);
 
